877172902aa82041c1eee564b507fe6651b2e96d
jungheeyoung <12young24@naver.com>
2015-10-03 17:37:18 +0900

    root redirect 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.shortcuts import redirect
+
+def redirect_home(request):
+	return redirect('blog.views.post_list')
 
 urlpatterns = [
+	url(r'^$', redirect_home),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
 ]
